15f3c6c7ac2caef3480e407bed6a299690b53b59
DiamondOsas <diamondosazuwa@gmail.com>
2026-03-22 16:49:40 +0100

    Adding GUI

--- firewall.bat
+++
@@ -1,2 +0,0 @@
-echo Allowing Firewall Rules
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-File \"%~dp0allow-firewall-rules.ps1\"'"  
--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Buidling Main and Backup...
-
-start "" /b cmd /c go build . >nul
-start "" /b cmd /c go build -o clipsync_backup.exe . >nul
